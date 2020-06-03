7606543ed4f0481a60417a2cb88fe177efd17b89
Sergey-17011984 <61842958+Sergey-17011984@users.noreply.github.com>
2020-06-04 02:13:26 +0300

    new dz

---
+++ 1.js
@@ -0,0 +1,34 @@
+
+
+
+
+//  a = 5;
+//  b = 85;
+// let c = a;вводим переменную c
+// a = b;
+// b = c;
+
+
+// a = a + b; 5 + 85 = 90;
+// b = a - b;90 -85 = 5 ;
+// a = b - a;90 - 5 = 85;
+
+
+//F = C × 1.8 + 32//
+// let c = 12 ;
+// let f = c * 1.8 + 32;
+// console.log(f);
+
+// var signedIn;
+//        console.log(signedIn);undefined
+
+// console.log("Hello" % 10 );   Nan
+
+// let number = +process.argv[2];
+// if (number % 2  ){
+//     console.log("Не чётное число" );
+// }else  {
+//     console.log("Чётноё число " );
+// }
+
+
